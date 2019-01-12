626e32bae976dba84c3fbb35dde7a6cd59a5cc2b
ranstotz <ubuntu@ip-172-26-6-176.ec2.internal>
2019-01-12 01:04:06 +0000

    updated server_live.py

---
+++ server_live.py
@@ -0,0 +1,89 @@
+# Surveillance Project
+# File: server_cv.py
+# Author: Ryan Anstotz
+# Description:
+#  Receives stream over socket, stores stream
+# =================================================================================
+
+# import necessary packages
+import os
+import sys
+import numpy as np
+import socket
+import pickle
+import struct
+#from picamera.array import PiRGBArray
+#from picamera import PiCamera
+import time 
+import cv2
+ 
+# main function
+def main(argv):
+
+    # set host and port for socket
+    HOST = '71.175.97.72'
+    PORT = 5050
+    
+    # initialize socket connections
+    print "Creating socket..."
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print "Socket created."
+
+    # bind socket
+    s.bind((HOST, PORT))
+    print "Socket bind complete."
+    s.listen(10)
+    print "Socket now listening."
+
+    # accept connection
+    conn, addr = s.accept()
+    print "Socket connected to IP:", addr[0]
+    
+    # data prep. 'L' stands for unsigned long
+    data = ""
+    payload_size = struct.calcsize("L")
+
+    # a test flag that will print something upon success
+    accepted_flag = False
+    
+    # accept data continuously
+    while (1):
+
+        # get and process the payload
+        while len(data) < payload_size:
+            data += conn.recv(4096)
+
+        packed_msg_size = data[:payload_size]
+        data = data[payload_size:]
+        msg_size = struct.unpack("L", packed_msg_size)[0]
+        while len(data) < msg_size:
+            data += conn.recv(4096)
+        frame_data = data[:msg_size]
+        data = data[msg_size:]
+
+        # load image
+        image = pickle.loads(frame_data)
+
+        if accepted_flag == False:
+            accepted_flag = True
+            print "*** the code appears to have worked ***"
+            print " this is the first row of the image array "
+            print image[0]
+        '''
+        # display the stream
+        cv2.imshow("Frame", image)
+        key = cv2.waitKey(1) & 0xFF
+
+        # if 'q' key pressed, break from loop
+        if key == ord("q"):
+            break
+        '''
+        
+    # return from main
+    return
+
+# execute 
+if __name__ == "__main__":
+    main(sys.argv[:])
+
+# end of file
