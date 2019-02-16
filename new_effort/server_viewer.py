b32c68a494dde6de92a11808035f2722b039c039
ranstotz <ubuntu@ip-172-26-6-176.ec2.internal>
2019-02-16 19:35:33 +0000

    new_effort appears to correctly stream

--- new_effort/server_viewer.py
+++ new_effort/server_viewer.py
@@ -23,8 +23,9 @@ while True:
         img = base64.b64decode(frame)
         npimg = np.fromstring(img, dtype=np.uint8)
         source = cv2.imdecode(npimg, 1)
-        cv2.imshow("Stream", source)
+        #cv2.imshow("Stream", source)
         cv2.waitKey(1)
+        print "captured data"
     except KeyboardInterrupt:
         cv2.destroyAllWindows()
         break
