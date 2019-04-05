a6220001c813e415a793c5cc336de66b1e104d9a
ranstotz <ryananstotz@gmail.com>
2019-04-04 20:14:51 -0400

    modified comments and moved old files.

--- pi_client.py
+++ pi_client.py
@@ -6,6 +6,8 @@ from picamera import PiCamera
 import time
 
 class clientStreamer(object):
+    ''' clientStreamer '''
+
     def __init__(self, ip, port):
         self.ip = ip
         self.port = port
@@ -14,19 +16,19 @@ class clientStreamer(object):
         self.context = zmq.Context()
         self.footage_socket = self.context.socket(zmq.PUB)
         print "Context and socket initialized. \nBinding to port."
-        #self.footage_socket.connect('tcp://localhost:5050')
+        #self.footage_socket.connect('tcp://localhost:5050')    # local testing
         self.footage_socket.connect('tcp://18.214.123.134:5050')
         print "Port initialized.\n"
         self.camera = ""
         self.rawCapture = ""
         
     def start_camera(self):
-        print "Initializing camera."
+        print "Initializing camera..."
         self.camera = PiCamera()
         self.camera.resolution = (640, 480)
         self.camera.framerate = 32
         self.rawCapture = PiRGBArray(self.camera, size=(640, 480))
-        time.sleep(0.1) # Warm up camera
+        time.sleep(0.1)    # Warm up camera
         print "Camera initialized."
     
     def begin_stream(self):
