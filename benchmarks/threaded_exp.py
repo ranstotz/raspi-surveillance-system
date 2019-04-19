2788cab74e3ec7545794e5e658a0df4b802459e4
ranstotz <ryananstotz@gmail.com>
2019-04-19 19:22:38 -0400

    added naive threading. single thread works but is very slow comparatively.

--- benchmarks/threaded_exp.py
+++ benchmarks/threaded_exp.py
@@ -15,10 +15,26 @@ from picamera import PiCamera
 import time 
 import cv2
 import argparse
+import threading
 
+
+def write_files(cv):
+    global shared_buffer
+
+    condition.acquire()
+    while len(shared_buffer) == 0:
+        condition.wait()
+    cv2.imwrite(shared_buffer[0][0], shared_buffer[0][1])
+    shared_buffer.pop()
+    print "wrote image"
+    condition.release()
+
+    
 # main function
-def main(argv):
+def main(cv, argv):
 
+    global shared_buffer
+    
     # get args
     ap = argparse.ArgumentParser()
     ap.add_argument("-f", "--frames", type=int, default=100)
@@ -43,16 +59,24 @@ def main(argv):
     # warm up camera
     time.sleep(0.8)
     print "Starting threaded video capture... "
-    
 
+    # initialize file-writing thread
+    fw_t = threading.Thread(target=write_files)
+    
+    file_name = ""
     frameCounter = 0
     # capture frames from camera
     while fps._numFrames < args["frames"]:
         frame = vs.read()
         fps.update()
+        print "length buf: ", len(shared_buffer)
         file_name = "output/test" + str(frameCounter) + ".jpg"
-        cv2.imwrite(file_name, frame)
+        condition.acquire()
+        shared_buffer.append((file_name, frame))
+        condition.notify()
+        condition.release()
         frameCounter += 1
+
     fps.stop()
 
     print "frames: ", fps._numFrames
@@ -63,6 +87,14 @@ def main(argv):
 
 
 if __name__ == "__main__":
-    main(sys.argv[:])
+    shared_buffer = []
+    condition = threading.Condition()
+    main_thread = threading.Thread(target=main, args=(condition, sys.argv[:],))
+    writer_thread = threading.Thread(target=write_files, args=(condition,))
+
+    main_thread.start()
+    writer_thread.start()
+    
+
 
 
