7e080a540c74a55cfbb2c90018b075e26ee90f3b
ranstotz <ryananstotz@gmail.com>
2019-05-21 23:43:11 -0400

    added main method to stream_video

--- src/stream_video.py
+++ src/stream_video.py
@@ -1,15 +1,22 @@
+import sys
+import os
+
 from pi_client import clientStreamer
 
-client = clientStreamer()
+def main(argv):
 
-client.connect_streaming_socket("18.214.123.134", "5050")
-'''
-client.connect_messaging_socket("*", "5051")
-message = ""
-while True:
+    client = clientStreamer()
+    
+    client.connect_streaming_socket("18.214.123.134", "5050")
+    '''
+    client.connect_messaging_socket("*", "5051")
+    message = ""
+    while True:
     message = client.receive_message()
     print message
-'''
-client.start_camera()
-client.begin_stream()
+    '''
+    client.start_camera()
+    client.begin_stream()
 
+if __name__ == "__main__":
+    main(sys.argv[:])
